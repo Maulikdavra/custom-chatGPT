03c8c520d99f589f65ed75dc572379ec542c8533
Maulik Davra <maulikdavra06@gmail.com>
2023-08-27 15:40:03 -0500

    final commit

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import openai
+from fastapi import FastAPI, Form, Request
+from typing import Annotated
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-chat_log = []
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-while True:
+chat_response = []
 
-    user_input = input()
 
-    if user_input.lower == 'stop':
-        break
+@app.get("/", response_class=HTMLResponse)
+async def chat_page(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "chat_responses": chat_response})
 
+
+chat_log = [{'role': 'system',
+             'content': 'You are a professional senior java developer, assist users with industry standard code practise \
+              and advice given at senior level'
+             }]
+
+
+@app.post("/", response_class=HTMLResponse)
+async def chat(request: Request, user_input: Annotated[str, Form()]):
     chat_log.append({'role': 'user', 'content': user_input})
+    chat_response.append(user_input)
 
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
@@ -19,4 +34,22 @@ while True:
 
     bot_response = response['choices'][0]['message']['content']
     chat_log.append({'role': 'assistant', 'content': bot_response})
-    print(bot_response)
+    chat_response.append(bot_response)
+    return templates.TemplateResponse("home.html", {"request": request, "chat_responses": chat_response})
+
+
+@app.get("/image", response_class=HTMLResponse)
+async def image_page(request: Request):
+    return templates.TemplateResponse("image.html", {"request": request})
+
+
+@app.post("/image", response_class=HTMLResponse)
+async def create_image(request: Request, user_input: Annotated[str, Form()]):
+    response = openai.Image.create(
+        prompt=user_input,
+        n=1,
+        size="512x512"
+    )
+
+    image_url = response['data'][0]['url']
+    return templates.TemplateResponse("image.html", {"request": request, "image_url": image_url})
